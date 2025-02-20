eed02ceb633c4906417535ebd9a87eb816de91e4
N. I. Md. Ashafuddula (RaZon) <md.ashafuddula@gmail.com>
2025-02-19 18:39:58 -0600

    Update PathDict.py

--- main/run/PathDict.py
+++ main/run/PathDict.py
@@ -1,24 +1,38 @@
 # ----------------------------------
-# This file is used to define the path of the data and the cache file
+# PathDict.py
 
-# Write dataset path in this format
-# DatasetName = {
-#     "train_label"   :   "",
-#     "train_path"    :   "",
-#     "val_label"     :   "",
-#     "val_path"      :   "",
-#     "test_label"    :   "",
-#     "test_path"     :   "",
-#     "cache_path"    :   "",
-#     "center_file"   :   "",
-#     "doctor_file"   :   "",
-#     "modal"         :   ["",]
-# }
+# Root paths
 
-# RootPath        = ""
-# CodePath        = ""
-# DocEvlPath      = ""
-# ExpFolder       = ""
-# pretrain_folder = ""
+RootPath = "/home/C00579118/SampleData_CoPAS"
+dataroot = "/home/C00579118/SampleData_CoPAS"
+CodePath = "/home/C00579118/CoPAS-0812"
+DocEvlPath = "/home/C00579118/CoPAS-0812/doctor_eval/"
+ExpFolder = "/home/C00579118/CoPAS-0812/experiment_folder/"
+pretrain_folder = "/home/C00579118/SampleData_CoPAS/model.pth"
 
-# -----------------------------------
+# Dataset configuration
+dataset_dict = {
+    'Internal': {  # Changed from 'internal' to 'Internal'
+        "train_path": f"{dataroot}/Internal",  # Changed from 'train' to 'Internal'
+        "train_label": f"{dataroot}/trainLabels.csv",
+        
+        "val_path": f"{dataroot}/validation",
+        "val_label": f"{dataroot}/validationLabels.csv",
+        
+        "test_path": f"{dataroot}/test",
+        "test_label": f"{dataroot}/testLabels.csv",
+        
+        "cache_path": f"{dataroot}/cache",
+        "center_file":   f"{dataroot}/center_file.csv",  # Optional: File related to the center
+        "doctor_file":   f"{dataroot}/doctor_file.csv",  # Optional: File related to the doctor           
+        
+        # Ensure sequence order matches your directories
+        "modal": [
+            "sag PDW",
+            "cor PDW",
+            "axi PDW",
+            "sag T2WI",
+            "cor T1WI"
+        ]
+    }
+}
